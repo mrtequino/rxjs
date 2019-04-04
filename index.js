abb5e12667ba68aed1a61cb95fe1ed2349e30c22
Diego Peñafiel <www.tequino@gmail.com>
2019-04-04 18:34:46 -0500

    04-04-2019 18:34

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const rxjs = require('rxjs')
+const { of, throwError } = require("rxjs");
+const { map, mergeMap, catchError, finalize } = require("rxjs/operators");
 
+let source$ = of(1, 2, 3);
+
+source$ = source$.pipe(
+  map(x => {
+    if (x == 2) {
+      return throwError("Soy un error lanzado concientemente");
+    } else {
+      return of(x);
+    }
+  }),
+  mergeMap(x => x),
+  catchError(err => {
+    throw "Me ejecuto cuando hay error";
+  }),
+  finalize(_ => {
+    console.log("Me ejecuto siempre");
+  })
+);
+
+source$.subscribe(
+  x => console.log(x),
+  err => console.log(err),
+  () => console.log("completo")
+);
